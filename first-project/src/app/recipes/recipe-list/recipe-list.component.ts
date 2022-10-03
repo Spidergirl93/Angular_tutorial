c151de90b3463c1cb7d9e37e19edce3f1b869a89
Maryam <71137692+Spidergirl93@users.noreply.github.com>
2022-10-03 16:31:48 +0200

    view the recipe list

--- first-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ first-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Pizza',
+      'The best food ever',
+      'https://ewscripps.brightspotcdn.com/dims4/default/7e89202/2147483647/strip/true/crop/1000x563+0+0/resize/1280x720!/quality/90/?url=http%3A%2F%2Fewscripps-brightspot.s3.amazonaws.com%2Fcc%2F4d%2Ff4e966f448bbb73d4ffbb1d396e1%2Fwptv-pizza-hut-pizza.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
